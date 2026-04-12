954330f73255e24d320da980851780095a0bfe73
akarnaukhova2003 <adelina.kzn15@gmail.com>
2026-04-12 20:08:37 +0300

    добавила одинаковый окрас и бутстреп

--- data/clade_analysis/AVES/Rtree/tree_build_good_leaves.r
+++ data/clade_analysis/AVES/Rtree/tree_build_good_leaves.r
@@ -2,76 +2,144 @@ library(ape)
 library(ggtree)
 library(ggplot2)
 library(ggnewscale)
-library(randomcoloR)
 library(phytools)
+library(randomcoloR)
 
-plot_annotated_tree_clusters <- function(tree_file, meta_file, taxlabel=TRUE){
+meta_path <- "/Users/abagavetdinova/Desktop/lab/Astroviridae_database/data/clade_analysis/AVES/Astroviridae_Aves_11042026_clusters.csv"
+
+all_meta <- read.csv(meta_path, stringsAsFactors = FALSE, check.names = FALSE)
+
+fix_na <- function(x) {
+  x[is.na(x)] <- "NA"
+  x
+}
+
+all_meta$aa83_1B <- fix_na(all_meta$aa83_1B)
+all_meta$aa90_1B <- fix_na(all_meta$aa90_1B)
+all_meta$nt86_1B <- fix_na(all_meta$nt86_1B)
+all_meta$nt80_1B <- fix_na(all_meta$nt80_1B)
+all_meta$Host <- fix_na(all_meta$Host)
+
+levels_f1 <- sort(unique(all_meta$aa83_1B))
+levels_f2 <- sort(unique(all_meta$aa90_1B))
+levels_f3 <- sort(unique(all_meta$nt86_1B))
+levels_f4 <- sort(unique(all_meta$nt80_1B))
+levels_host <- sort(unique(all_meta$Host))
+
+set.seed(123)
+
+make_map <- function(levels) {
+  cols <- distinctColorPalette(length(levels))
+  setNames(cols, levels)
+}
+
+color_map_f1 <- make_map(levels_f1)
+color_map_f2 <- make_map(levels_f2)
+color_map_f3 <- make_map(levels_f3)
+color_map_f4 <- make_map(levels_f4)
+color_map_host <- make_map(levels_host)
+
+plot_annotated_tree_clusters <- function(tree_file, meta_file, taxlabel = TRUE) {
   
   tree <- read.tree(tree_file)
   tree$edge.length[is.na(tree$edge.length)] <- 0
   tree$tip.label <- sapply(strsplit(tree$tip.label, "/"), `[`, 1)
+  
   tree_rooted <- midpoint.root(tree)
   
-  info <- read.csv(meta_file, sep=",", stringsAsFactors = FALSE, check.names = FALSE)
+  info <- read.csv(meta_file, stringsAsFactors = FALSE, check.names = FALSE)
   colnames(info) <- gsub(" ", "", colnames(info))
   rownames(info) <- info$id
   
-  info$aa83_1B[is.na(info$aa83_1B)] <- "NA"
-  info$aa90_1B[is.na(info$aa90_1B)] <- "NA"
-  info$nt86_1B[is.na(info$nt86_1B)] <- "NA"
-  info$nt80_1B[is.na(info$nt80_1B)] <- "NA"
-  info$Host[is.na(info$Host)] <- "NA"
+  fix_na <- function(x) {
+    x[is.na(x)] <- "NA"
+    x
+  }
+  
+  info$aa83_1B <- fix_na(info$aa83_1B)
+  info$aa90_1B <- fix_na(info$aa90_1B)
+  info$nt86_1B <- fix_na(info$nt86_1B)
+  info$nt80_1B <- fix_na(info$nt80_1B)
+  info$Host <- fix_na(info$Host)
   info$sequence_nm[is.na(info$sequence_nm)] <- "unknown"
   
-  cluster1 <- data.frame(AA83 = factor(info$aa83_1B), row.names = info$id)
-  cluster2 <- data.frame(AA90 = factor(info$aa90_1B), row.names = info$id)
-  cluster3 <- data.frame(NT86 = factor(info$nt86_1B), row.names = info$id)
-  cluster4 <- data.frame(NT80 = factor(info$nt80_1B), row.names = info$id)
-  host     <- data.frame(Host = factor(info$Host), row.names = info$id)
+  cluster1 <- data.frame(AA83 = factor(info$aa83_1B, levels = levels_f1), row.names = info$id)
+  cluster2 <- data.frame(AA90 = factor(info$aa90_1B, levels = levels_f2), row.names = info$id)
+  cluster3 <- data.frame(NT86 = factor(info$nt86_1B, levels = levels_f3), row.names = info$id)
+  cluster4 <- data.frame(NT80 = factor(info$nt80_1B, levels = levels_f4), row.names = info$id)
+  host <- data.frame(Host = factor(info$Host, levels = levels_host), row.names = info$id)
   
-  colors_f1 <- distinctColorPalette(length(unique(cluster1$AA83)))
-  colors_f2 <- distinctColorPalette(length(unique(cluster2$AA90)))
-  colors_f3 <- distinctColorPalette(length(unique(cluster3$NT86)))
-  colors_f4 <- distinctColorPalette(length(unique(cluster4$NT80)))
-  colors_host <- distinctColorPalette(length(unique(host$Host)))
+  p <- ggtree(tree_rooted, size = 0.7)
   
-  p <- ggtree(tree_rooted, size=0.7)
+  df <- p$data
+  df$bootstrap <- suppressWarnings(as.numeric(df$label))
+  p$data <- df
   
-  if(taxlabel){
+  if (taxlabel) {
     tip_labels_map <- setNames(info$sequence_nm, info$id)
-    tip_colors_map <- setNames(colors_host[host$Host], rownames(host))
     
-    p <- p + geom_tiplab(aes(label = tip_labels_map[.data$label]), offset=0.02, size=1.2) 
+    p <- p + 
+      geom_tiplab(
+        aes(label = tip_labels_map[.data$label]),
+        offset = 0.02,
+        size = 1.2
+      ) +
+      geom_point2(
+        aes(subset = !isTip & !is.na(bootstrap), fill = bootstrap),
+        shape = 21,
+        size = 2,
+        color = "black"
+      ) +
+      scale_fill_gradient(
+        low = "white",
+        high = "hotpink",
+        name = "Bootstrap"
+      ) +
+      new_scale_fill()
   } else {
     p <- p + geom_treescale()
   }
   
-  label_theme <- theme(axis.text.x = element_text(angle=45, hjust=0, vjust=0, size=0.5))
+  theme_small_x <- theme(axis.text.x = element_blank())
   
-  p1 <- gheatmap(p, cluster1, width=0.05, offset=0.12, colnames=TRUE, font.size=2, colnames_position="top") +
-    scale_fill_manual(values=colors_f1, guide="none") + label_theme + new_scale_fill()
+  p1 <- gheatmap(p, cluster1, width = 0.05, offset = 0.12,
+                 colnames = TRUE, font.size = 2, colnames_position = "top") +
+    scale_fill_manual(values = color_map_f1, guide = "none", na.value = "grey90") +
+    theme_small_x + new_scale_fill()
   
-  p1 <- gheatmap(p1, cluster2, width=0.05, offset=0.24, colnames=TRUE, font.size=2, colnames_position="top") +
-    scale_fill_manual(values=colors_f2, guide="none") + label_theme + new_scale_fill()
+  p1 <- gheatmap(p1, cluster2, width = 0.05, offset = 0.24,
+                 colnames = TRUE, font.size = 2, colnames_position = "top") +
+    scale_fill_manual(values = color_map_f2, guide = "none", na.value = "grey90") +
+    theme_small_x + new_scale_fill()
   
-  p1 <- gheatmap(p1, cluster3, width=0.05, offset=0.36, colnames=TRUE, font.size=2, colnames_position="top") +
-    scale_fill_manual(values=colors_f3, guide="none") + label_theme + new_scale_fill()
+  p1 <- gheatmap(p1, cluster3, width = 0.05, offset = 0.36,
+                 colnames = TRUE, font.size = 2, colnames_position = "top") +
+    scale_fill_manual(values = color_map_f3, guide = "none", na.value = "grey90") +
+    theme_small_x + new_scale_fill()
   
-  p1 <- gheatmap(p1, cluster4, width=0.05, offset=0.48, colnames=TRUE, font.size=2, colnames_position="top") +
-    scale_fill_manual(values=colors_f4, guide="none") + label_theme + new_scale_fill()
+  p1 <- gheatmap(p1, cluster4, width = 0.05, offset = 0.48,
+                 colnames = TRUE, font.size = 2, colnames_position = "top") +
+    scale_fill_manual(values = color_map_f4, guide = "none", na.value = "grey90") +
+    theme_small_x + new_scale_fill()
   
-  p1 <- gheatmap(p1, host, width=0.05, offset=0.60, colnames=TRUE, font.size=2, colnames_position="top") +
-    scale_fill_manual(values=colors_host, name="Host") + label_theme
+  p1 <- gheatmap(p1, host, width = 0.05, offset = 0.60,
+                 colnames = TRUE, font.size = 2, colnames_position = "top") +
+    scale_fill_manual(values = color_map_host, name = "Host", na.value = "grey90") +
+    theme_small_x
   
   return(p1)
 }
 
-tree_file <- "/Users/abagavetdinova/Desktop/lab/Astroviridae_database/data/clade_analysis/AVES/Aves_2_97_translated_align_new.nwk"
-meta_file <- "/Users/abagavetdinova/Desktop/lab/Astroviridae_database/data/clade_analysis/AVES/Astroviridae_Aves_05042026_clusters.csv"
+tree_file <- "/Users/abagavetdinova/Desktop/lab/Astroviridae_database/data/clade_analysis/AVES/Aves_1B_97_translated_tree.nwk"
 
-p <- plot_annotated_tree_clusters(tree_file, meta_file)
+p <- plot_annotated_tree_clusters(tree_file, meta_path)
 print(p)
 
-ggsave("/Users/abagavetdinova/Desktop/lab/Astroviridae_database/data/clade_analysis/AVES/2_97_tree.png",
-       p, width=14, height=10, dpi=300)
+ggsave(
+  "/Users/abagavetdinova/Desktop/lab/Astroviridae_database/data/clade_analysis/AVES/1B_97_tree.png",
+  p,
+  width = 14,
+  height = 10,
+  dpi = 300
+)
 
